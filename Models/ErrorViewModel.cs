445c76a612b1f8c44c292cabcc9bae01ff5aa52e
Pierre Lance Francisco <12010143@benilde.edu.ph>
2022-07-06 14:16:51 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CCAP_Inventory_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
