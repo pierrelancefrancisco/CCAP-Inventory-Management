07080a7050d6a11b092266db1d4a0d10ae9ead7b
Julian Carl Angeles <juliancarlangeles@gmail.com>
2022-08-01 21:17:52 +0800

    Add migration

--- Migrations/20220801131451_UpdateProductMigration.Designer.cs
+++ Migrations/20220801131451_UpdateProductMigration.Designer.cs
@@ -3,14 +3,16 @@ using CCAP_Inventory_Management.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CCAP_Inventory_Management.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220801131451_UpdateProductMigration")]
+    partial class UpdateProductMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220801131451_UpdateProductMigration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CCAP_Inventory_Management.Migrations
+{
+    public partial class UpdateProductMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Products");
+        }
+    }
+}
